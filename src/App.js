502a396f40bb6bc581b812668779b90a44fdacb7
Krishandeep Singh <krishandeep17@gmail.com>
2023-08-07 13:04:24 +0530

    add useEffect and useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,49 @@
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading", // 'loading', 'error', 'ready', 'active', 'finished'
+  index: 0,
+  answer: null,
+  points: 0,
+  highScore: 0,
+  secondsRemaining: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "received_data": {
+      return { ...state, questions: action.payload, status: "ready" };
+    }
+    case "failed": {
+      return { ...state, status: "error" };
+    }
+
+    default:
+      throw new Error(`Unknown action ${action.type}`);
+  }
+};
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const {
+    questions,
+    status,
+    index,
+    answer,
+    points,
+    highScore,
+    secondsRemaining,
+  } = state;
+
+  useEffect(() => {
+    fetch("http://localhost:5000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "received_data", payload: data }))
+      .catch((err) => dispatch({ type: "failed" }));
+  }, []);
+
   return <div className="app">React Quiz App</div>;
 };
 
