c75a3a3a2986e2bb24f74d7e87043a96629fdd24
Krishandeep Singh <krishandeep17@gmail.com>
2023-08-13 15:15:36 +0530

    change className

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -2,7 +2,7 @@ const NextButton = ({ dispatch, index, numQuestions }) => {
   if (index === numQuestions - 1) {
     return (
       <button
-        className="btn btn-ui"
+        className="btn btn-right"
         onClick={() => dispatch({ type: "finish" })}
       >
         Finish
@@ -12,7 +12,7 @@ const NextButton = ({ dispatch, index, numQuestions }) => {
 
   return (
     <button
-      className="btn btn-ui"
+      className="btn btn-right"
       onClick={() => dispatch({ type: "next_question" })}
     >
       Next
